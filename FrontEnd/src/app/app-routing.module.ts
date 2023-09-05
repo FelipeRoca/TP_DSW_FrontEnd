df10111411abcd91cbec47b4b2a8d4c588d51546
FelipeRoca <feliroca01@gmail.com>
2023-09-05 19:17:48 -0300

    login a sigin

--- FrontEnd/src/app/app-routing.module.ts
+++ FrontEnd/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './modules/login/login.component';
+import { SiginComponent } from './modules/sigin/sigin.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    {path:'', redirectTo:'/iniciar-sesion', pathMatch:'full'},
+    {path: 'iniciar-sesion', component:LoginComponent},
+    {path: 'registrarse', component:SiginComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
