83b55cab04c2ccf96eb43f0219d121f7fcdc04e3
AlexisMerce <alexis.am.2001@gmail.com>
2023-10-05 19:33:41 -0300

    se agrego navbar al log-in y sign-in

--- FrontEnd/src/app/modules/login/login.component.ts
+++ FrontEnd/src/app/modules/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MenuItem } from 'primeng/api';
 
 @Component({
   selector: 'app-login',
@@ -16,6 +17,55 @@ export class LoginComponent {
       passwordInput.type = 'password';
     }
   }
+
+  /* Barra de navegacion */
+
+  items: MenuItem[] | undefined;
+
+   ngOnInit() {
+        this.items = [
+            {
+                label: 'Home',
+                icon: 'pi pi-fw pi-file',
+                items: [] 
+            },
+            {
+              icon: 'pi pi-fw pi-pencil',
+              disabled: true,
+              label: 'Reseñas',
+                items: [
+                    {
+                        label: 'Crear Reseña',
+                        icon: 'pi pi-fw pi-align-left'
+                    },
+                    {
+                        label: 'Leer Reseñas',
+                        icon: 'pi pi-fw pi-align-right'
+                    },
+                    {
+                        label: 'Eliminar Reseña',
+                        icon: 'pi pi-fw pi-align-center'
+                    },
+                ]
+            },
+            {
+                label: 'Iniciar Sesion',
+                routerLink: ['.'],
+                icon: 'pi pi-fw pi-user',
+                items: []
+            },
+            {
+                label: 'Registrarse',
+                routerLink: ['/registrarse'],
+                icon: 'pi pi-fw pi-calendar',
+                items: []
+            },
+            {
+                label: 'Salir',
+                icon: 'pi pi-fw pi-power-off'
+            }
+        ];
+    }
 
 }
 
--- FrontEnd/src/app/modules/sigin/sigin.component.ts
+++ FrontEnd/src/app/modules/sigin/sigin.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MenuItem } from 'primeng/api';
 
 @Component({
   selector: 'app-sigin',
@@ -6,5 +7,52 @@ import { Component } from '@angular/core';
   styleUrls: ['./sigin.component.css']
 })
 export class SiginComponent {
+
+  items: MenuItem[] | undefined;
+
+ngOnInit() {
+        this.items = [
+            {
+                label: 'Home',
+                icon: 'pi pi-fw pi-file',
+                items: [] 
+            },
+            {
+              icon: 'pi pi-fw pi-pencil',
+              disabled: true,
+              label: 'Reseñas',
+                items: [
+                    {
+                        label: 'Crear Reseña',
+                        icon: 'pi pi-fw pi-align-left'
+                    },
+                    {
+                        label: 'Leer Reseñas',
+                        icon: 'pi pi-fw pi-align-right'
+                    },
+                    {
+                        label: 'Eliminar Reseña',
+                        icon: 'pi pi-fw pi-align-center'
+                    },
+                ]
+            },
+            {
+                label: 'Iniciar Sesion',
+                routerLink: ['/iniciar-sesion'],
+                icon: 'pi pi-fw pi-user',
+                items: []
+            },
+            {
+                label: 'Registrarse',
+                routerLink: ['.'],
+                icon: 'pi pi-fw pi-calendar',
+                items: []
+            },
+            {
+                label: 'Salir',
+                icon: 'pi pi-fw pi-power-off'
+            }
+        ];
+    }
 
 }
